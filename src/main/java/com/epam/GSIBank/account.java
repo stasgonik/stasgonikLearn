fc1d213f601b4b1524705959331c00eaad728bdd
stasgonik <alaroskael@gmail.com>
2020-05-23 14:19:59 +0300

    win?

--- src/main/java/com/epam/GSIBank/account.java
+++
@@ -1,9 +0,0 @@
-package com.epam.GSIBank;
-
-import com.epam.GSIBank.user;
-
-public class account {
-    private int id;
-    private user master;
-    private currency accountCurrency;
-}
--- src/main/java/epam/Gonyk/Test.java
+++ src/main/java/epam/Gonyk/Test.java
@@ -1,7 +1,7 @@
 package epam.Gonyk;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String []args) {
+        System.out.println("Hello FU!");
     }
 }
