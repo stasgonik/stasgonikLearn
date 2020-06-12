c44ed7cceac41b67dadc3e970c889e59d5121d5f
stasgonik <alaroskael@gmail.com>
2020-06-12 17:29:44 +0300

    start h2

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,5 +1,14 @@
+import java.sql.*;
 public class application {
     public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            // add application code here
+            conn.close();
+        }
+        catch (Exception ex) {
+            ex.getMessage();
+        }
         account gloria = account.createAccount();
         System.out.println(gloria.toString());
     }
